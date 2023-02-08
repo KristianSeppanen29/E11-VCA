ac01d78fc84bf21d9354c24cd2268f5583e58375
KristianSeppanen29 <124214251+KristianSeppanen29@users.noreply.github.com>
2023-02-08 15:27:03 -0800

    Create LabWeek4.py

---
+++ LabWeek4.py
@@ -0,0 +1,22 @@
+import time
+import board
+import busio
+from digitalio import DigitalInOut, Direction, Pull
+from adafruit_pm25.i2c import PM25_I2C
+import serial
+uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout = 0.25)
+from adafruit_pm25.uart import PM25_UART
+pm25 = PM25_UART(uart, reset_pin)
+print("Found PM2.5 sensor, reading data...")
+
+while True:
+    time.sleep(1)
+
+    try:
+        aqdata = pm25.read()
+        # print(aqdata)
+    except RuntimeError:
+        print("Unable to read from sensor, retrying...")
+        continue
+
+  
