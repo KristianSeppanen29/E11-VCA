9a7a54a199fc111d6e398bee089adfa55a1360e7
KristianSeppanen29 <124214251+KristianSeppanen29@users.noreply.github.com>
2023-02-08 15:37:21 -0800

    Update LabWeek4.py

--- LabWeek4.py
+++ LabWeek4.py
@@ -3,6 +3,7 @@ import board
 import busio
 from digitalio import DigitalInOut, Direction, Pull
 from adafruit_pm25.i2c import PM25_I2C
+reset_pin = None
 import serial
 uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout = 0.25)
 from adafruit_pm25.uart import PM25_UART
