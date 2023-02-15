4f9c78acddfbe993b2798fd65b41e5aacbb8028d
KristianSeppanen29 <124214251+KristianSeppanen29@users.noreply.github.com>
2023-02-15 14:47:05 -0800

    Create Lab 5 example.py

---
+++ Lab 5 example.py
@@ -0,0 +1,19 @@
+import random
+import time
+import argparse
+import sys
+
+print(sys.argv)
+
+
+start_time = time.time()
+run_time = sys.argv[1]
+itime = start_time
+
+
+
+while True < (start_time + run_time)
+    itime = time.time()
+    idata = ramdom.random()
+    print(itime, idata)
+    time.sleep(1)
