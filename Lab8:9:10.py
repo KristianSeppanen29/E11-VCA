b5d4543656068860b88c4c616234cabe9208b912
AdithyaAyanam <124214191+AdithyaAyanam@users.noreply.github.com>
2023-03-22 15:18:45 -0700

    file

---
+++ Lab8:9:10.py
@@ -0,0 +1,47 @@
+# coding=utf-8
+ 
+import RPi.GPIO as GPIO
+import datetime
+import time
+metadata = "radiation"
+f = open("radiation_sensor_data", "w", newline = '') 
+writer = csv.writer(f)
+writer.writerow(meta_data)
+data = GPIO.HIGH
+counts = 0
+ 
+def my_callback(channel):
+     global counts 
+     counts+=1
+    
+
+  
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(5, GPIO.IN)
+GPIO.add_event_detect(5, GPIO.FALLING, callback=my_callback)
+
+
+ 
+
+starttime = time.time
+while time.time<=start_time+60:
+    message = raw_input('\nPress any key to exit.\n')
+    currenttime = time.time
+    counts = counts
+    writer = csv.writer(f)
+    writer.writerow(currenttime, counts)
+    writer.writerow(data)
+    time.sleep(10)
+    counts = 0
+
+
+    
+    
+
+
+#It wants to give out the time and counts detected during that time
+    # Set up the system so that it detects falling edges 
+    #Set a function that does something when it detects a falling edge. 
+    #While loop for an infinite time in that while loop, you want to have a global count variable and you want it to sleep for a minute and then record about the current time and how many counts there are. 
+    #You need to set global variable so that for the next run of the loop, the count starts from 0. 
+    # Before reseting global variable, write current time and how many counts of time there are.
