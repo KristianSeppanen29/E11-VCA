20a4c6cc21007a2bfb56e3baa564d9d1cfac85b3
AdithyaAyanam <124214191+AdithyaAyanam@users.noreply.github.com>
2023-03-22 15:51:58 -0700

    Update Lab8:9:10.py

--- Lab8:9:10.py
+++ Lab8:9:10.py
@@ -27,7 +27,8 @@ GPIO.add_event_detect(5, GPIO.FALLING, callback=my_callback)
  
 
 start_time = time.time
-big_time = int(start_time)+60
+start_time+=60
+big_time = start_time
 while time.time<=big_time:
     message = raw_input('\nPress any key to exit.\n')
     time.sleep(10)
