745c307a8036a967203e3fc5fba75587a297acc0
AdithyaAyanam <124214191+AdithyaAyanam@users.noreply.github.com>
2023-03-24 20:26:37 -0700

    Add files via upload

--- Lab8:9:10.py
+++ Lab8:9:10.py
@@ -3,45 +3,43 @@
 import RPi.GPIO as GPIO
 import datetime
 import time
-import csv
-counts = 0
-meta_data = ["time", "counts"]
+metadata = "radiation"
 f = open("radiation_sensor_data", "w", newline = '') 
 writer = csv.writer(f)
 writer.writerow(meta_data)
-
+data = GPIO.HIGH
 
  
 def my_callback(channel):
-     global counts 
-     counts+=1
-     print(counts)
+    if GPIO.input(channel) == GPIO.HIGH:
+        global counts 
+        counts+=1
     
 
   
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(6, GPIO.IN)
-GPIO.add_event_detect(6, GPIO.FALLING, callback=my_callback)
+GPIO.setup(5, GPIO.IN)
+GPIO.add_event_detect(5, GPIO.BOTH, callback=my_callback)
 
 
  
 
-start_time = time.time()
-start_time += 120
-big_time = start_time
-while time.time()<=big_time:
+starttime = time.time
+while time.time<=start_time+60:
+    message = raw_input('\nPress any key to exit.\n')
+    writer.writerow(data)
+    global counts
+    counts = counts
     time.sleep(10)
-    currenttime = time.time()
-    writer.writerow([currenttime, counts])
-    counts = 0
-f.close()
-
-
-    
-    
+f = open("radiation_sensor_data.csv", "w", newline = '')
+writer = csv.writer(f)
+writer.writerow(currenttime, count)
+currenttime = time.time
+count = counts
 
 
 #It wants to give out the time and counts detected during that time
+
     # Set up the system so that it detects falling edges 
     #Set a function that does something when it detects a falling edge. 
     #While loop for an infinite time in that while loop, you want to have a global count variable and you want it to sleep for a minute and then record about the current time and how many counts there are. 
